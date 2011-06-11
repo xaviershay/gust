c45e8aad82cd2dd92574218639217cb138aac6ad
Xavier Shay <xavier@rhnh.net>
2011-06-11 11:16:27 +1000

    Refactor routing to look similiar, preparing for refactor.

--- lib/gust_application.rb
+++ lib/gust_application.rb
@@ -13,24 +13,21 @@ class GustApplication
   def call(env)
     request = Rack::Request.new(env)
 
-    gust_regex = %r{/gusts/([0-9a-f]{32})$}
-    gust_file_regex = %r{/gusts/([0-9a-f]{32})/(.+)$}
+    gust_regex      = %r{^/gusts/([0-9a-f]{32})$}
+    gust_file_regex = %r{^/gusts/([0-9a-f]{32})/(.+)$}
+    home_regex      = %r{^/$}
 
-    response = if request.path_info == '/'
+    response = if request.path_info == '/' && request.get?
       Controllers::Gust.new(@config, request.params).new
     elsif request.path_info =~ gust_file_regex
-      id       = request.path_info[gust_file_regex, 1]
-      filename = request.path_info[gust_file_regex, 2]
-
-      Controllers::Gust.new(@config, request.params).raw(id, filename)
-    elsif request.path_info =~ gust_regex
-      id = request.path_info[gust_regex, 1]
-
-      if request.post? || request.put?
-        Controllers::Gust.new(@config, request.params).put(id)
-      else
-        Controllers::Gust.new(@config, request.params).show(id)
-      end
+      matches = request.path_info.match(gust_file_regex).captures
+      Controllers::Gust.new(@config, request.params).raw(*matches)
+    elsif request.path_info =~ gust_regex && (request.post? || request.put?)
+      matches = request.path_info.match(gust_regex).captures
+      Controllers::Gust.new(@config, request.params).put(*matches)
+    elsif request.path_info =~ gust_regex && request.get?
+      matches = request.path_info.match(gust_regex).captures
+      Controllers::Gust.new(@config, request.params).show(*matches)
     else
        Rack::Response.new(["NOT FOUND"], 404)
     end
